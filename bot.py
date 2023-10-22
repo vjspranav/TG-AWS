a98bf7381261013a79cebb6a0f475c6c06b36aa1
vjspranav <pranavasri@live.in>
2023-10-23 01:13:31 +0530

    Improvise status check

--- bot.py
+++ bot.py
@@ -14,8 +14,8 @@ import time
 from telegram.ext import Updater
 from telegram.ext import CommandHandler, ConversationHandler
 
-token=""
-instance=''
+token="1486232044:AAH1DTSvr9ySFKkfIH3LjNpQ2mFwA8ERL5U"
+instance='i-0c65c4c3127999e85'
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -28,96 +28,96 @@ print(bot.get_me())
 updater = Updater(token=token, use_context=True)
 dispatcher = updater.dispatcher
 
+SERVER_STOPPED = 0
+SERVER_STARTING = 1
+SERVER_STARTED = 2
+SERVER_RUNNING = 3
+
+statuses = {
+    "stopped": SERVER_STOPPED,
+    "starting": SERVER_STARTING,
+    "started": SERVER_STARTED,
+    "running": SERVER_RUNNING
+}
+
+def __get_status__():
+    statuses = subprocess.check_output("aws ec2 describe-instance-status --include-all-instances", shell=True)
+    statuses = json.loads(statuses)
+    for status in statuses['InstanceStatuses']:
+        if status['InstanceId'] == instance:
+            curStatus = status['InstanceState'][' Name']
+            return statuses.get(curStatus, curStatus)
+    return None
+
+def __start_server__():
+    os.system("aws ec2 start-instances --instance-ids " + instance)
+
+def __stop_server__():
+    os.system("aws ec2 stop-instances --instance-ids " + instance)
+
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hi! Welcome to Valo Bot")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hi! Welcome to Valo Bot, I am a very useful bot that helps you get one step closer to wasting your life.")
 
-def starts(update,context):
-    msg = context.bot.send_message(chat_id=update.effective_chat.id, text="Checking Server Status...")
-    # Check status of instance
-    statuses = subprocess.check_output("aws ec2 describe-instance-status --include-all-instances", shell=True)
-    statuses = json.loads(statuses)
-    # Format:
-    #     {
-    #     "InstanceStatuses": [
-    #         {
-    #             "AvailabilityZone": "ap-south-1b",
-    #             "InstanceId": "i-0c65c4c3127999e85",
-    #             "InstanceState": {
-    #                 "Code": 80,
-    #                 "Name": "stopped"
-    #             },
-    #             "InstanceStatus": {
-    #                 "Status": "not-applicable"
-    #             },
-    #             "SystemStatus": {
-    #                 "Status": "not-applicable"
-    #             }
-    #         }
-    #     ]
-    # }
 
-    # get the entry with instance id
-    for status in statuses['InstanceStatuses']:
-        if status['InstanceId'] == instance:
-            if status['InstanceState']['Name'] == 'started':
-                msg.edit_text("Server is already started")
-            elif status['InstanceState']['Name'] == 'stopped':
-                os.system("aws ec2 start-instances --instance-ids " + instance)
-                msg.edit_text("Server is starting")
-                break
-            else:
-                msg.edit_text("Server is in " + status['InstanceState']['Name'] + " state")
-            break
-    
-    # Keep checking status of instance until it is started
-    while True:
-        statuses = subprocess.check_output("aws ec2 describe-instance-status --include-all-instances", shell=True)
-        statuses = json.loads(statuses)
-        if statuses['InstanceStatuses'][0]['InstanceState']['Name'] == 'running':
-            msg.edit_text("Server Started")
-            break
-        time.sleep(2)
-
-    # os.system("aws ec2 stop-instances --instance-ids " + instance)
-    # msg = context.bot.send_message(chat_id=update.effective_chat.id, text="Checking Server Status [1/4]...")
-    # time.sleep(30)
-    # os.system("aws ec2 start-instances --instance-ids " + instance)
-    # msg.edit_text("Preparing Server [2/4]...")
-    # time.sleep(30)
-    # os.system("aws ec2 stop-instances --instance-ids " + instance)
-    # msg.edit_text("Starting Server [3/4]...")
-    # time.sleep(45)
-    # os.system("aws ec2 start-instances --instance-ids " + instance)
-    # time.sleep(30)
-    # msg.edit_text("Server Started [4/4]")
-
-def stops(update,context):
-    msg = context.bot.send_message(chat_id=update.effective_chat.id, text="Checking Server Status...")
-    statuses = subprocess.check_output("aws ec2 describe-instance-status --include-all-instances", shell=True)
-    statuses = json.loads(statuses)
-    for status in statuses['InstanceStatuses']:
-        if status['InstanceId'] == instance:
-            if status['InstanceState']['Name'] == 'stopped':
-                msg.edit_text("Server is already stopped")
-            elif status['InstanceState']['Name'] == 'running' or status['InstanceState']['Name'] == 'started':
-                os.system("aws ec2 stop-instances --instance-ids " + instance)
-                msg.edit_text("Server is stopping")
-                break
-            else:
-                msg.edit_text("Server is in " + status['InstanceState']['Name'] + " state")
-            break
-
-    while True:
-        statuses = subprocess.check_output("aws ec2 describe-instance-status --include-all-instances", shell=True)
-        statuses = json.loads(statuses)
-        if statuses['InstanceStatuses'][0]['InstanceState']['Name'] == 'stopped':
+def check_server_status(update, context, action, fstart=False):
+    msg = context.bot.send_message(chat_id=update.effective_chat.id, text=f"Checking Server Status for {action}...")
+    status = __get_status__()
+    if status == SERVER_STARTED and action == "start":
+        if not fstart:
+            msg.edit_text("Server is already started")
+    elif status == SERVER_STOPPED and action == "stop":
+        if not fstart:
+            msg.edit_text("Server is already stopped")
+    elif status == SERVER_STARTED and action == "stop":
+        __stop_server__()
+        if not fstart:
+            msg.edit_text("Server is stopping")
+        while __get_status__() != SERVER_STOPPED:
+            time.sleep(2)
+        if not fstart:
             msg.edit_text("Server Stopped")
-            break
-        time.sleep(5)
-    # os.system("aws ec2 stop-instances --instance-ids " + instance)
-    # context.bot.send_message(chat_id=update.effective_chat.id, text="Server stopped")
+    elif status == SERVER_STOPPED and action == "start":
+        __start_server__()
+        if not fstart:
+            msg.edit_text("Server is starting")
+        while __get_status__() != SERVER_STARTED:
+            time.sleep(2)
+        if not fstart:
+            msg.edit_text("Server Started")
+    elif status is None:
+        if not fstart:
+            msg.edit_text("Server not found")
+    else:
+        if not fstart:
+            msg.edit_text(f"Server is in {status} state")
 
-functions=[start, starts, stops]
+def starts(update, context):
+    check_server_status(update, context, "start")
+
+def stops(update, context):
+    check_server_status(update, context, "stop")
+
+def restart(update, context):
+    check_server_status(update, context, "stop")
+    check_server_status(update, context, "start")
+
+# sometimes a single start doesn't work, so we try twice start, stop, start
+def fstart(update, context):
+    check_server_status(update, context, "start")
+    check_server_status(update, context, "stop")
+    check_server_status(update, context, "start")
+
+functions=[start, starts, stops, restart, fstart]
+
+def help(update, context):
+    # Send all available commands
+    helptxt = "Available commands:\n"
+    helptxt += "/starts - Start the server\n"
+    helptxt += "/stops - Stop the server, it can take upto 3-4 minutes\n"
+    helptxt += "/help - Show this help message\n"
+    context.bot.send_message(chat_id=update.effective_chat.id, text=helptxt)
+
+functions.append(help)
 
 for function in functions:
     handler = CommandHandler(function.__name__, function)
